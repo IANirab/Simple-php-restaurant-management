a8506c018807b42fd21eceb0ba3830bd1a8a4f53
Istiaq Ahmmed Nirab <40653908+IANirab@users.noreply.github.com>
2019-01-05 18:49:09 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "id7546657_123456");
-define("DB_PASS", "nirabAB00$$");
-define("DB_NAME", "id7546657_food");
+define("DB_USER", ""); // your database username
+define("DB_PASS", ""); // your database password
+define("DB_NAME", ""); // your database name
 
